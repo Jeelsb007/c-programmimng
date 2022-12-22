932e9dfb76720ae7365cc50728ecaad2fe1121de
jeelsb007 <jeelsbhuva0077@gmail.com>
2022-12-22 16:35:25 +0530

    mathwematical process

---
+++ ch3/JEELS1.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+main(){
+       int a=10,b=5;
+       clrscr();
+
+       printf("sum of %i and %i=%d\n",a,b,a+b);
+       printf("substraction of%i and %i=%d\n",a,b,a-b);
+       printf("mutiply of%i and %i=%d\n",a,b,a*b);
+       printf("division of%i and %i=%d\n",a,b,a/b);
+       printf("remainder of%i and %i=%d\n",a,b,a%b);
+       getch();
+
+
+
+
+}
