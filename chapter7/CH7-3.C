e0f73c3f4b6ee435dd47779c4d4c760c315588b1
Jeelsb007 <jeelsbhuva007@gmail.com>
2023-01-04 17:03:32 +0530

    my chapter7

---
+++ chapter7/CH7-3.C
@@ -0,0 +1,15 @@
+
+#include<stdio.h>
+#include<conio.h>
+main(){
+       int i=1,n;
+       clrscr();
+       printf("ENTER VALUE OF n:");
+       scanf("%d",&n);
+       while(i<=n){
+	 printf("%d\n",i);
+	 i++;
+
+       }
+       getch();
+}
---
+++ chapter7/CH7-7.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+main(){
+       int i=2000;                                                                                                                                                                       ;
+       clrscr();
+       while(i<=3000){
+	 printf("%d\n",i);
+	 i+=4;
+
+       }
+       getch();
+}
